34365c440e04e05b769b8ddc2c7f15dff2ecaaa1
Theo Otto <theorochaotto@gmail.com>
2024-05-29 19:46:14 -0300

    Função remover item quase pronta
    Para finalizar dependo do resto do grupo

--- remover.py
+++ remover.py
@@ -1,30 +1,63 @@
-def remover_item():
-    categoria = str(input("Bebida\nEntradas\nPratos Principais\nSobremesas\nO item que deseja excluir faz parte de qual categoria?\n"))
+import cardapio as cp
+
+def remover_item(cardapio):
+    cp.exibirCategorias()
+    categoria = str(input("O item que deseja excluir faz parte de qual categoria?\n"))
     categoria.lower()
 
-    validacao = False
+    validacaoCategoria = False
 
-    while validacao == False:
+    while validacaoCategoria == False:
         print("CATEGORIA INVÁLIDA!\nPor favor digite novamente\n")
-        categoria = str(input("Bebida\nEntradas\nPratos Principais\nSobremesas\nO item que deseja excluir faz parte de qual categoria?\n"))
+        cp.exibirCategorias()
+        categoria = str(input("\nO item que deseja excluir faz parte de qual categoria?\n"))
         categoria.lower()
 
-        if categoria == "bebida" or categoria == "entradas" or categoria == "pratos principais" or categoria == "Sobremesas":
-            validacao = True
+        if cp.verificarCategoria(categoria) == True:
+            validacaoCategoria = True
         else:
-            validacao = False
+            validacaoCategoria = False
     
-    if categoria == "bebida":
-        refri,suco,cafe,cha,
+    if categoria == "bebidas":
+        cp.exibirSubCategorias()
+        subcategoria = str(input("O item que deseja excluir faz parte de qual subcategoria?"))
+        subcategoria.lower()
 
-    elif categoria == "entrada":
-        a
+        validacaoSubcategoria = False
 
-    elif categoria == "pratos principais":
-        a
+        while validacaoSubcategoria == False:
+            print("SUBCATEGORIA INVÁLIDA!\nPor favor digite novamente\n")
+            cp.exibirSubCategorias()
+            subcategoria = str(input("\nO item que deseja excluir faz parte de qual subcategoria?\n"))
+            subcategoria.lower()
 
-    elif categoria == "sobremesas":
-        a
+            if cp.verificarCategoria(subcategoria) == True:
+                validacaoSubcategoria = True
+            else:
+                validacaoSubcategoria = False
+        
+        if subcategoria == "refrigerantes":
+            cp.exibirItens()
+            item = str(input("Qual item você deseja excluir?"))
+            item.lower()
+
+            validacaoItem = False
+
+            while validacaoItem == False:
+                print("SUBCATEGORIA INVÁLIDA!\nPor favor digite novamente\n")
+                cp.exibirSubCategorias()
+                item = str(input("\nQual item você deseja excluir?\n"))
+                item.lower()
+
+                if cp.verificarCategoria(item) == True:
+                    validacaoItem = True
+                else:
+                    validacaoItem = False
+            
+            if item == "coca-cola":
+                excluido = cardapio.pop("coca-cola")
+                print(f"Item {excluido} removido com sucesso!")
+
 
 
 remover_item()
