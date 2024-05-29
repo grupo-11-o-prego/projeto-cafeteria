7e612e2bbd2c8598415d27172c170b5c8687a0db
Eduardo <lourenco.e@pucpr.edu.br>
2024-05-28 21:37:47 -0300

    add - cardapio - salvar()

--- cardapio.py
+++ cardapio.py
@@ -1,9 +1,14 @@
 import json
+
 arquivo = 'cardapio.txt'
 
 # abre o arquivo e armazena os valores em uma variavel chamada 'cardapio_conteudo'
-with open(arquivo, 'r') as file:
-  cardapio_conteudo = file.read()
+with open(arquivo, 'r') as arquivo_aberto:
+  cardapio_conteudo = arquivo_aberto.read()
  
 # transforma o conteudo do arquivo em um dicionário
 cardapio = json.loads(cardapio_conteudo)    
+
+def salvar(): 
+  with open(arquivo, 'w') as arquivo_aberto:
+    arquivo_aberto.write(str(cardapio))
