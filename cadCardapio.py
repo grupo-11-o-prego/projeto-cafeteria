ffb07c3fec0f0e1807681cf938da519a24df1747
Hector Vieira Saldivar <87508113+hector-vs@users.noreply.github.com>
2024-06-04 21:28:37 -0300

    feat: finalizando função de cadastro de item no cardápio

--- cadCardapio.py
+++ cadCardapio.py
@@ -1,5 +1,15 @@
 import cardapio as cp
 
 def cadCardapio():
-    return False
-#A fazer
+    validCat = False
+    validSub = False
+    print("========== CADASTRO DE PRODUTO NO CARDÁPIO ==========")
+    categoria = cp.escolherCategoria()
+    subcategoria = cp.escolherSubCategoria(categoria)
+    nome = str(input("Informe o nome do produto a ser cadastrado: ")).lower()
+    valor = float(input(f"Informe o valor do produto '{nome}': "))
+    estoque = int(input(f"Informe a quantidade em estoque do produto '{nome}': "))
+    cp.cardapio[categoria][subcategoria][nome] = {'valor': valor, 'estoque': estoque}
+    print(f"Adicionado: \n ===> {nome} <=== \n{cp.cardapio[categoria][subcategoria][nome]}")
+    cp.salvar()
+cadCardapio()
