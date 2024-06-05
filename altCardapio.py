a34d6c4466bba85d458211b42f8f60fe5356a5c0
Eduardo <lourenco.e@pucpr.edu.br>
2024-06-04 21:13:51 -0300

    [add] buscar cardapio

--- altCardapio.py
+++ altCardapio.py
@@ -3,3 +3,9 @@ import cardapio as cp
 categoria = cp.escolherCategoria()
 subcategoria = cp.escolherSubCategoria(categoria)
 produto = cp.escolherProduto(categoria, subcategoria)
+
+print("-- Opções ----------------------------------------")
+print(" [1] - Editar valor")
+print(" [2] - Editar estoque")
+print("--------------------------------------------------")
+
--- buscarcardapio.py
+++ buscarcardapio.py
@@ -1,11 +1,23 @@
 import cardapio as cp
 def buscar(cardapio):
     
-    produto_desejado = input("Qual o produto desejado ?")
-    for cardapio_k, cardapio_v in cp.cardapio.items():
-         for produtos_k, produtos_v in cardapio_v.items():
-            if produto_desejado in produtos_k:
-               print(f"{produto_desejado} = {produtos_v} reais")
+   produto_desejado = input("Qual o produto desejado ? ")
+   produto_achado = False
+   for cardapio_k, cardapio_v in cp.cardapio.items():
+      for produtos_k, produtos_v in cardapio_v.items():
+         for produto_k, produto_v in produtos_v.items():
+            if produto_desejado in produto_k:
+               print("--------------------------------------------------")
+               print(f"produto: {produto_k}")
+               print(f"valor: R$ {produto_v["valor"]:6.2f}")
+               print(f"estoque: {produto_v["estoque"]} unidades")
+               print(f"categoria: {cardapio_k}")
+               print(f"subcategoria: {produtos_k}")
+               produto_achado = True
+
+   if(produto_achado == False):
+      print(f"> o produto '{produto_desejado}' não existe!")
+         
 
 
 buscar(cp.cardapio)
